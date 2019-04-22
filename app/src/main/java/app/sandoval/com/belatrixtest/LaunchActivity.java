322dc032c8ca659f97c94d3838b0d14a41cb46f6
jsandoval <jsandoval@wearenovae.com>
2019-04-22 07:48:43 -0500

    First Apps features 0.0.1 - Launch Activity - Change Icon - Splash Screen Image

---
+++ app/src/main/java/app/sandoval/com/belatrixtest/LaunchActivity.java
@@ -0,0 +1,33 @@
+package app.sandoval.com.belatrixtest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_launch);
+
+        int TIME_OUT = 2000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(LaunchActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, TIME_OUT);
+
+    }
+}
